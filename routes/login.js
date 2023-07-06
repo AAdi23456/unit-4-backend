601e73f33ae42355b6d63eaa18b7dca898ca0098
AAdi23456 <118712556+AAdi23456@users.noreply.github.com>
2023-07-06 22:22:08 +0530

    login bugs fixed

--- routes/login.js
+++ routes/login.js
@@ -4,18 +4,20 @@ const bcrypt = require("bcrypt")
 const reg_model = require("../model/reg")
 
 const jwt = require("jsonwebtoken")
-const validate=require("../middleware/postmiddleware")
+
 Authorization.post("/signup", async (req, res) => {
   const { name, email, password } = req.body
+  console.log(req.body);
   try {
     const userr=await reg_model.findOne({email})
+    console.log(userr);
     if(userr){
-      return res.status(400).json({error:"user already exist"})
+      return res.status(200).json({msg:"user already exist"})
     }
     bcrypt.hash(password, 8, async (err, hash) => {
       const datatodb = new reg_model({ name, email, password: hash })
       await datatodb.save()
-      res.json("data saved")
+      res.json({msg:"regestration success"})
       console.log(err);
     })
 
@@ -30,20 +32,25 @@ Authorization.post("/login", async (req, res) => {
   try {
     const user = await reg_model.findOne({ email })
     console.log(user);
-    if (user) {
+    if(!user){
+      return res.status(200).json({"msg": "Please regeister first"} )
+    }
+    
       bcrypt.compare(password, user.password, (err, result) => {
         console.log(err);
         console.log(password);
 
-        if (result) {
-          res.status(200).json({ "msg": "Login successfull!", "token": jwt.sign({ "email": user.email }, "masai"),"name":user.name})
-        } else {
-          res.status(400).json({ "msg": "Wrong Credentials" })
+        if (!result) {
+         return res.status(400).json({ "msg": "Wrong Credentials" })
+        
         }
+        return  res.status(200).json({ "msg": "Login successfull!", "token": jwt.sign({ "email": user.email }, "masai"),"name":user.name})
       });
-    }
+    
+    
   } catch (err) {
     res.status(400).json({ "msg": err.message })
+    
   }
 })
 
